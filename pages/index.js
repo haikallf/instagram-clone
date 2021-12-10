7435e371e80dd247b74a9a1b9ad7df776e76a20a
haikallf <haikallzrd@gmail.com>
2021-12-11 04:53:46 +0700

    stories and story component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Feed from "../components/Feed";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Header />
 
       {/* Feed */}
+      <Feed />
 
       {/* Model */}
     </div>
