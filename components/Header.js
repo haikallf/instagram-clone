7d38c220cbfe70d6ee8ba54f10aab17fdd61bac1
haikallf <haikallzrd@gmail.com>
2021-12-11 03:08:57 +0700

    header component

---
+++ components/Header.js
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import React from "react";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+
+import { HomeIcon } from "@heroicons/react/solid";
+
+function Header() {
+  return (
+    <div>
+      <div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
+        {/* Left */}
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative w-10 lg:hidden cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle */}
+        <div className="max-w-sx">
+          {/* Middle - Search Container*/}
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-gray-500
+            focus:border-gray-500"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="flex">
+          <HomeIcon className="h-10 w-10" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
